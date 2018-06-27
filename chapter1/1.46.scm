42a82fcaf46e1457d11ebebcb25d13217bf09251
gustav <40590729+Whalesea-Gustav@users.noreply.github.com>
2018-06-27 20:07:33 +0800

    Create 1.46.scm

---
+++ chapter1/1.46.scm
@@ -0,0 +1,10 @@
+(define (iterative-improve close-enough? improve)
+    (lambda (first-guess)
+        (define (try guess)
+            (let ((next (improve guess)))
+                (if (close-enough? guess next)
+                    next
+                    (try next))))
+        (try first-guess)))
+        
+ 迭代式改进
