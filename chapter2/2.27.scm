8f39af993f6a7369fd2193f524480af1ce1536b5
gustav <40590729+Whalesea-Gustav@users.noreply.github.com>
2018-06-29 18:46:53 +0800

    Create 2.27.scm

---
+++ chapter2/2.27.scm
@@ -0,0 +1,8 @@
+(define (deep-reverse tree)
+    (cond ((null? tree)         ; 空树
+            '())
+          ((not (pair? tree))   ; 叶子
+            tree)
+          (else
+            (reverse (list (deep-reverse (car tree))            ; 递归地逆序左右子树
+                           (deep-reverse (cadr tree)))))))
